520186bfa589546ed00f0f927109ed4577223c86
CoderParth <ghantigalli@gmail.com>
2023-09-22 23:32:01 +1000

    Add/Allowed_Hosts

--- traffic_dashboard/settings.py
+++ traffic_dashboard/settings.py
@@ -96,7 +96,7 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 
-ALLOWED_HOSTS = [config('ALLOWED_HOSTS')]
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost').split(',')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
